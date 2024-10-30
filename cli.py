b7c01eeb3087c9acbd3f03434df9f51c54ed0450
zhmf7408 <kido@canway.net>
2024-10-30 17:16:13 +0800

    feature 修改munchkin url地址

--- cli.py
+++ cli.py
@@ -61,7 +61,8 @@ class BootStrap(object):
                 'password': server_settings.rabbitmq_password,
                 'queues': [
                     'pilot'
-                ]
+                ],
+                'bot_id': str(server_settings.munchkin_bot_id)
             }
         }
         with open('data/endpoints.yml', 'w', encoding='utf-8') as f:
